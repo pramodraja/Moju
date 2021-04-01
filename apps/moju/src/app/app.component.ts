95fab5205aed4d2fc1c4ba4523cfb2c35ba9702d
pramodraja <pramod2001@gmail.com>
2021-04-01 12:23:19 -0400

    initial commit

---
+++ apps/moju/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tasti-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'moju';
+}
