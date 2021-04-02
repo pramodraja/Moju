9ffb5e28bf369fd5cd4f4f0e65e7566a69b9cdba
pramodraja <pramod2001@gmail.com>
2021-04-01 20:03:29 -0400

    Adding images and css

--- apps/moju/src/app/app.module.ts
+++ apps/moju/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule,
+    FlexLayoutModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
